eead8d6709e00fe009068ecc77d7a2f3f057d3ba
mahdi zohrevand <zohrevand.mahdi@gmail.com>
2022-11-12 22:35:12 +0330

    add check box

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,9 @@ import {
   Chip,
   Autocomplete,
   TextField,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
 } from "@mui/material";
 import ColorSchemePicker from "../src/ThemeToggleButton";
 
@@ -27,61 +30,79 @@ const Home: NextPage = () => {
           alignItems: "center",
         }}
       >
-        <Box bgcolor="background.paper" sx={{ p: 1 }}>
-          <Box sx={{ py: 2, mx: "auto" }}>
-            <Box sx={{ pb: 4 }}>
-              <ColorSchemePicker />
-            </Box>
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 4, mb: 1 }}>
-              <Button color="primary" variant="contained">
-                primary
-              </Button>
-              <Button variant="outlined">primary</Button>
-              <Button>primary</Button>
-            </Box>
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 4, mb: 1 }}>
-              <Button color="secondary" variant="contained">
-                secondary
-              </Button>
-              <Button color="secondary" variant="outlined">
-                secondary
-              </Button>
-              <Button color="secondary">secondary</Button>
-            </Box>
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 4, mb: 1 }}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={10}
-                  label="Age"
-                  size="small"
-                  //onChange={handleChange}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 4, mb: 1 }}>
-              <Chip label="badge" />
-            </Box>
-            <Autocomplete
-              size="small"
-              disablePortal
-              options={["one", "two", "three"]}
-              onChange={(event, value) => {
-                console.log(value);
-              }}
-              sx={{
-                width: 200,
-                minWidth: 200,
-              }}
-              renderInput={(params) => <TextField {...params} label="input" />}
-            />
+        <Box
+          sx={{
+            py: 2,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+          }}
+        >
+          <Box sx={{ pb: 4 }}>
+            <ColorSchemePicker />
           </Box>
+          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 4, mb: 1 }}>
+            <Button color="primary" variant="contained">
+              primary
+            </Button>
+            <Button variant="outlined">primary</Button>
+            <Button>primary</Button>
+          </Box>
+          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 4, mb: 1 }}>
+            <Button color="secondary" variant="contained">
+              secondary
+            </Button>
+            <Button color="secondary" variant="outlined">
+              secondary
+            </Button>
+            <Button color="secondary">secondary</Button>
+          </Box>
+          <div>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={10}
+                label="Age"
+                size="small"
+                //onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div>
+            <Chip label="badge" />
+          </div>
+          <Autocomplete
+            size="small"
+            disablePortal
+            options={["one", "two", "three"]}
+            onChange={(event, value) => {
+              console.log(value);
+            }}
+            sx={{
+              width: 200,
+              minWidth: 200,
+            }}
+            renderInput={(params) => <TextField {...params} label="input" />}
+          />
+
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Label"
+            />
+            <FormControlLabel
+              disabled
+              control={<Checkbox />}
+              label="Disabled"
+            />
+          </FormGroup>
         </Box>
       </Box>
     </Container>
